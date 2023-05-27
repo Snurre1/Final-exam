0beb9dd7137f65c6b19fd1356728fbf9ad83463a
Thomas Henriksen <thomashenriksen832@hotmail.com>
2023-05-27 18:09:44 +0200

    Update index.jsx

--- src/components/venues/searchbarvenuelist/index.jsx
+++ src/components/venues/searchbarvenuelist/index.jsx
@@ -11,7 +11,7 @@ export const SearchResultsList = ({results}) => {
                     <S.SearchVenueImageContainer><S.SearchVenueImage src={result.media} alt={result.name}></S.SearchVenueImage></S.SearchVenueImageContainer>
                     <S.SearchCountryWrapper>Country: {result.location.country}</S.SearchCountryWrapper>
                     <S.SearchCityWrapper>City: {result.location.city} </S.SearchCityWrapper>
-                   <Link to={id} style={{margin: "auto"}}><S.SearchViewListingBtn>View Listing</S.SearchViewListingBtn></Link>
+                   <Link to={id} style={{margin: "auto"}}><S.SearchViewListingBtn>View Venue</S.SearchViewListingBtn></Link>
                     </S.SearchVenueWrapper>
                 )
             })
